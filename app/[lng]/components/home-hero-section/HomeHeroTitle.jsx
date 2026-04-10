7bb9a9843adf728b262a4fd1ed4f0555fc378ad2
nikola <nikolamilojevic1981@gmail.com>
2026-04-10 11:13:14 +0700

    fix: home hero section title styling

--- app/[lng]/components/home-hero-section/HomeHeroTitle.jsx
+++ app/[lng]/components/home-hero-section/HomeHeroTitle.jsx
@@ -2,21 +2,22 @@ import styles from './HomeHeroSection.module.scss';
 import EstText from '@/app/[lng]/components/home-hero-section/HomeHeroEstText';
 
 const HomeTitle = ({ title, subtitle, estText }) => {
-  const splitTitle = title => {
-    const mid = Math.ceil(title.length / 2);
-    const firstHalf = title.slice(0, mid);
-    const secondHalf = title.slice(mid);
-    return [firstHalf, secondHalf];
-  };
-
-  const [left, right] = splitTitle(title);
+  // const splitTitle = title => {
+  //   const mid = Math.ceil(title.length / 2);
+  //   const firstHalf = title.slice(0, mid);
+  //   const secondHalf = title.slice(mid);
+  //   return [firstHalf, secondHalf];
+  // };
+  //
+  // const [left, right] = splitTitle(title);
 
   return (
     <div className={styles.TitleWrapper}>
       <div className={styles.Title}>
         <h2>
-          <span className={styles.LeftTitle}>{left}</span>
-          <span className={styles.RightTitle}>{right}</span>
+          {title}
+          {/*<span className={styles.LeftTitle}>{left}</span>*/}
+          {/*<span className={styles.RightTitle}>{right}</span>*/}
         </h2>
       </div>
       {estText && <EstText estText={estText} media={'mobile'} />}
